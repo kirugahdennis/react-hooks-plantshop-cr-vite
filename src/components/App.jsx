5e26105517f5fcd30737517157ced980954c29be
Denis <kirugahdennis@gmail.com>
2026-06-07 16:05:06 +0300

    Add Changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
+import PlantList from "./PlantList";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+  fetch("http://localhost:6001/plants")
+.then((res) => res.json())
+.then((data) => setPlants(data));
+},[]);
+
+function addPlant(newPlant) {
+  setPlants([...plants, newPlant]);
+}
+function toggleStock(id) {
+  const UpdatedPlants= plants.map((plant) =>
+   plant.id === id
+?{...plant, inStock: !plant.inStock}
+: plant
+);
+} 
+const filteredPlants = plants.filter((plant) =>
+plant.name.toLowerCase().includes(search.toLowerCase()));
   return (
-  <></>
+  <main>
+    <header/>
+
+      <input
+      type= "text"
+      placeholder = "Search plants.."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      />
+      <NewPlantForm onAddPlant = {addPlants} />
+      <PlantList
+      plants={filteredPlants}
+      onToggleStock={toggleStock}
+      />
+  </main>
   );
 }
 
--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const[plants, setPlants] = useState([]);
+  const[search, setSearch] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((res) => res.json())
+    .then((data) => setPlants(data));
+  },[]);
+
+function handleAddPlant(newPlant) {
+  setPlants([...plants, newPlant]);
+}
+
+function handleToggleStock(id){
+  const updatedPlants = plants.map((plant) =>
+  plant.id === id
+? {...plant, inStock: !plant.inStock}
+: plant
+);
+setPlants(updatedPlants);
+
+}
+
+const filteredPlants = plants.filter((plant) =>
+plant.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search
+      search={search}
+      onSearchChange={setSearch}
+       />
+      <PlantList 
+      plants={filteredPlants}
+      onToggleStock={handleToggleStock}/>
     </main>
   );
 }
